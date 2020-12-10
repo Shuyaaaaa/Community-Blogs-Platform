b3ad0a3f2b02ff77856f93d1205d4b86cfd5d97d
Shuya Wang <shuyawang@Shuyas-Mac.local>
2020-12-10 00:41:31 -0500

    add dynamoDB

--- flaskBlog/venv/bin/activate.fish
+++ flaskBlog/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/shuyawang/Desktop/flaskBlog/venv"
+set -gx VIRTUAL_ENV "/Users/shuyawang/Desktop/ece1779ass3/flaskBlog/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
